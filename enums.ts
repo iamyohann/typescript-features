813f8f123545f0ad363ef0df1cb351523bbb85ae
Yohann <1330242+iamyohann@users.noreply.github.com>
2018-03-03 22:44:57 +1100

    More enums (#9)

--- enums.ts
+++ enums.ts
@@ -17,3 +17,11 @@ enum Foo {
 }
 
 console.log(Foo.Bar, Foo.Baz); // 12,19
+
+// Const Enums
+
+const enum ConstantDirections {
+    Up, Down, Left, Right,
+}
+
+console.log(ConstantDirections.Up === 0); // true
